1a5c5335e401e22642d68995244e6c18c3ad2c73
Kritib Bhattarai <bhatkr01@kritibs-mbp.lc.luther.edu>
2021-08-31 23:53:59 -0500

    restart

--- project/css_website/settings.py
+++ project/css_website/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-from django_heroku import django_heroku
+import django_heroku
 import django_database_url
 from decouple import config
 
